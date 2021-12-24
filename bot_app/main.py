ed49d5a3b91c9def02deb1ba0fb7b19f6c9dbc2f
M Atheer <masreplay@gmail.com>
2021-12-24 11:14:45 +0300

    Validate bot user input

--- bot_app/main.py
+++ bot_app/main.py
@@ -4,6 +4,7 @@ import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 from aiogram.utils import executor
@@ -21,6 +22,10 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+branches = ["امنية", "برمجيات", "وسائط", "ذكاء", "شبكات", "نظم"]
+stages = ["ثاني", "اول", "رابع", "ثالث"]
+shifts = ["صباحي", "مسائي"]
+
 
 # States
 class Form(StatesGroup):
@@ -29,8 +34,8 @@ class Form(StatesGroup):
     shift = State()  # Will be represented in storage as 'Form:gender'
 
 
-@dp.message_handler(commands='start')
-async def cmd_start(message: types.Message):
+@dp.message_handler(commands='schedule')
+async def cmd_schedule(message: types.Message):
     """
     Conversation's entry point
     """
@@ -43,6 +48,29 @@ async def cmd_start(message: types.Message):
     markup.add("شبكات", "نظم")
 
     await message.reply("اختر الفرع", reply_markup=markup)
+
+
+# You can use state '*' if you need to handle all states
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info('Cancelling state %r', current_state)
+    # Cancel state and inform user about it
+    await state.finish()
+    # And remove keyboard (just in case)
+    await message.reply('تم الالغاء', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(lambda message: message.text not in branches, state=Form.branch)
+async def process_branch_invalid(message: types.Message):
+    return await message.reply("اختر من القائمة")
 
 
 @dp.message_handler(state=Form.branch)
@@ -61,11 +89,13 @@ async def process_branch(message: types.Message, state: FSMContext):
     await message.reply("اختر المرحلة", reply_markup=markup)
 
 
+@dp.message_handler(lambda message: message.text not in stages, state=Form.stage)
+async def process_stage_invalid(message: types.Message):
+    return await message.reply("اختر من القائمة")
+
+
 @dp.message_handler(state=Form.stage)
 async def process_stage(message: types.Message, state: FSMContext):
-    """
-    Process user name
-    """
     async with state.proxy() as data:
         data['stage'] = message.text
 
@@ -77,8 +107,13 @@ async def process_stage(message: types.Message, state: FSMContext):
     await message.reply("اختر نوع الدراسة", reply_markup=markup)
 
 
+@dp.message_handler(lambda message: message.text not in shifts, state=Form.shift)
+async def process_shift_invalid(message: types.Message):
+    return await message.reply("اختر من القائمة")
+
+
 @dp.message_handler(state=Form.shift)
-async def process_gender(message: types.Message, state: FSMContext):
+async def process_shift(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['shift'] = message.text
 
@@ -107,4 +142,4 @@ async def process_gender(message: types.Message, state: FSMContext):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=False)
