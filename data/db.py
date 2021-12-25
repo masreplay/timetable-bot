3e6f2bdd82060c3c63082d7f36aff6728e65194a
M Atheer <masreplay@gmail.com>
2021-12-25 23:35:35 +0300

    update absolute path

--- data/db.py
+++ data/db.py
@@ -4,10 +4,13 @@ from functools import lru_cache
 from asc_scrapper.extract_asc_schedule import load_data
 from asc_scrapper.schemas import *
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+
 
 @lru_cache()
 def get_cards():
-    return load_data(os.path.dirname(os.path.abspath('cards.json')), Card)
+    return load_data(os.path.join(BASE_DIR, 'cards.json'), Card)
 
 
 @lru_cache()
