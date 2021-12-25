4564db22f92808043b3161776c3ba4da49e6a9bd
M Atheer <masreplay@gmail.com>
2021-12-25 23:30:59 +0300

    update absolute path

--- data/db.py
+++ data/db.py
@@ -4,44 +4,42 @@ from functools import lru_cache
 from asc_scrapper.extract_asc_schedule import load_data
 from asc_scrapper.schemas import *
 
-dir_name = os.path.dirname(os.path.abspath('cards.json') + "/")
-
 
 @lru_cache()
 def get_cards():
-    return load_data(dir_name + 'cards.json', Card)
+    return load_data(os.path.dirname(os.path.abspath('cards.json')), Card)
 
 
 @lru_cache()
 def get_teachers():
-    return load_data(dir_name + 'teachers.json', Teacher)
+    return load_data(os.path.dirname(os.path.abspath('teachers.json')), Teacher)
 
 
 @lru_cache()
 def get_lessons():
-    return load_data(dir_name + 'lessons.json', Lesson)
+    return load_data(os.path.dirname(os.path.abspath('lessons.json')), Lesson)
 
 
 @lru_cache()
 def get_classrooms():
-    return load_data(dir_name + 'classrooms.json', Classroom)
+    return load_data(os.path.dirname(os.path.abspath('classrooms.json')), Classroom)
 
 
 @lru_cache()
 def get_periods():
-    return load_data(dir_name + 'periods.json', Period)
+    return load_data(os.path.dirname(os.path.abspath('periods.json')), Period)
 
 
 @lru_cache()
 def get_days_def():
-    return load_data(dir_name + 'daysdefs.json', Day)
+    return load_data(os.path.dirname(os.path.abspath('daysdefs.json')), Day)
 
 
 @lru_cache()
 def get_subjects():
-    return load_data(dir_name + 'subjects.json', Subject)
+    return load_data(os.path.dirname(os.path.abspath('subjects.json')), Subject)
 
 
 @lru_cache()
 def get_classes():
-    return load_data(dir_name + 'classes.json', Class)
+    return load_data(os.path.dirname(os.path.abspath('classes.json')), Class)
