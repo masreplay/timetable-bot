e8665476ad532f3c930dec0573b26d09cba14a8e
M Atheer <masreplay@gmail.com>
2022-01-09 01:42:55 +0300

    Add to endpoints

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,5 +1,5 @@
 from app.api.api_v1.endpoints import (users, roles, periods, auth, job_titles, stages, departments, branches, buildings,
-                                      rooms, floors)
+                                      rooms, floors, subjects, lessons, cards)
 from app.core.utils.utils import APIPermissionsRouter
 
 api_router = APIPermissionsRouter()
@@ -15,3 +15,6 @@ api_router.include_router(stages.router, prefix="/stages", tags=["stages"])
 api_router.include_router(buildings.router, prefix="/buildings", tags=["buildings"])
 api_router.include_router(rooms.router, prefix="/rooms", tags=["rooms"])
 api_router.include_router(floors.router, prefix="/floors", tags=["floors"])
+api_router.include_router(subjects.router, prefix="/subjects", tags=["subjects"])
+api_router.include_router(lessons.router, prefix="/lessons", tags=["lessons"])
+api_router.include_router(cards.router, prefix="/cards", tags=["cards"])
