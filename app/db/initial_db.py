20b0dfea65faa1a55e7cda0fa7944f8062871854
M Atheer <masreplay@gmail.com>
2022-01-12 22:33:47 +0300

    update init db

--- app/db/initial_db.py
+++ app/db/initial_db.py
@@ -17,24 +17,26 @@ def init() -> None:
 
 
 def init_super_admin(db):
-    super_admin_role = crud.role.create(
-        db=db, obj_in=schemas.RoleCreate(
-            ar_name="مسؤول",
-            en_name="SUPER ADMIN",
-            permissions=super_admin_permissions,
+    user = crud.user.get_by_email(db, email=settings().FIRST_SUPERUSER)
+    if not user:
+        super_admin_role = crud.role.create(
+            db=db, obj_in=schemas.RoleCreate(
+                ar_name="مسؤول",
+                en_name="SUPER ADMIN",
+                permissions=super_admin_permissions,
+            )
         )
-    )
-    user: schemas.User = crud.user.create(
-        db=db, obj_in=schemas.UserCreate(
-            email=settings().FIRST_SUPERUSER,
-            password=settings().FIRST_SUPERUSER_PASSWORD,
-            color='#000000',
-            gender=None,
-            en_name="SUPER ADMIN",
-            name="مسؤول",
-            role_id=super_admin_role.id,
+        crud.user.create(
+            db=db, obj_in=schemas.UserCreate(
+                email=settings().FIRST_SUPERUSER,
+                password=settings().FIRST_SUPERUSER_PASSWORD,
+                color='#000000',
+                gender=None,
+                en_name="SUPER ADMIN",
+                name="مسؤول",
+                role_id=super_admin_role.id,
+            )
         )
-    )
 
 
 def main() -> None:
