511d4362d7c46a66ba29e79208907130ad134c5f
M Atheer <masreplay@gmail.com>
2022-02-09 20:21:26 +0300

    Implement testing

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,6 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-app = FastAPI()
-
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
+from app.main import app
 
 client = TestClient(app)
 
