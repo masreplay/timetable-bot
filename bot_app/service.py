724261e680941f4e65689bf409f486029f710b84
M Atheer <masreplay@gmail.com>
2022-02-26 00:38:15 +0300

    get schedule image url

--- bot_app/service.py
+++ bot_app/service.py
@@ -5,12 +5,25 @@ import requests
 from app import schemas
 from app.core.config import settings
 
-
 # Fixme: implement error handler and handel base url as subclass from requests.Session
+from asc_scrapper.test import ImageUrl
+
 
 def get_stages(branch_name: str) -> schemas.Paging[schemas.Stage]:
-    response = requests.get(url=f"{settings().FAST_API_HOST}/stages?branch_name={branch_name}")
+    response = requests.get(
+        url=f"{settings().FAST_API_HOST}/stages",
+        params=dict(branch_name=branch_name),
+    )
+
     return schemas.Paging[schemas.Stage].parse_obj(response.json())
+
+
+def get_schedule_image_url(stage_id: UUID) -> str:
+    response = requests.get(
+        url=f"{settings().FAST_API_HOST}/schedule/image",
+        params={"stage_id": stage_id}
+    )
+    return ImageUrl.parse_obj(response.json()).url
 
 
 def get_stage_by_name(*, branch_name: str, stage_name: str) -> schemas.Stage | None:
