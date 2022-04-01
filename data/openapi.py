c8d206750273eb39a97ca86ef588a82f695a9c09
M Atheer <masreplay@gmail.com>
2022-04-01 15:41:21 +0300

    format openapi

--- data/openapi.py
+++ data/openapi.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 file_path = Path("openapi.json")
 openapi_content = json.loads(file_path.read_text())
-
+version = "v1"
 for path_data in openapi_content["paths"].values():
     for operation in path_data.values():
         try:
             tag = operation["tags"][0]
-            operation_id = operation["operationId"].replace("v1_", "")
+            operation_id = operation["operationId"].replace(f"{version}_", "")
             to_remove = f"{tag}-"
             new_operation_id = operation_id[len(to_remove):]
             operation["operationId"] = new_operation_id
